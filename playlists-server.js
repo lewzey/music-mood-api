71e08af44aa277d0602b676843bf3a302d88eda7
Louise Chill Monungolh <113127477+lewzey@users.noreply.github.com>
2026-02-18 21:42:15 -0700

    Update playlists-server.js

--- playlists-server.js
+++ playlists-server.js
@@ -21,42 +21,5 @@ routes.handleOtherMoods(supabase,app);
 
 let port = process.env.PORT
 app.listen(port, () => {
-    
     console.log("Server running at port= " + port);
-    /* if running on local server: 
-    console.log('http://localhost:8080/api/artists');
-    console.log('http://localhost:8080/api/artists/129');
-    console.log('http://localhost:8080/api/artists/averages/129');
-    console.log('http://localhost:8080/api/genres');
-    console.log('http://localhost:8080/api/songs');
-    console.log('http://localhost:8080/api/songs/1010');
-    console.log('http://localhost:8080/api/songs/sort/artist');
-    console.log('http://localhost:8080/api/songs/sort/year');
-    console.log('http://localhost:8080/api/songs/sort/duration');
-    console.log('http://localhost:8080/api/songs/search/begin/love');
-    console.log('http://localhost:8080/api/songs/search/any/love');
-    console.log('http://localhost:8080/api/songs/search/year/2017');
-    console.log('http://localhost:8080/api/songs/search/year/2016/2018');
-    console.log('http://localhost:8080/api/songs/artist/149');
-    console.log('http://localhost:8080/api/songs/genre/115');
-    console.log('http://localhost:8080/api/playlists');
-    console.log('http://localhost:8080/api/playlists/3');
-    console.log('http://localhost:8080/api/mood/dancing/5');
-    console.log('http://localhost:8080/api/mood/dancing/500');
-    console.log('http://localhost:8080/api/mood/happy/8');
-    console.log('http://localhost:8080/api/mood/happy');
-    console.log('http://localhost:8080/api/mood/coffee/10');
-    console.log('http://localhost:8080/api/mood/studying/15');
-
-    console.log('errors:')
-    console.log('http://localhost:8080/api/artists/sdfjkhsdf');
-    console.log('http://localhost:8080/api/songs/sjdkfhsdkjf');
-    console.log('http://localhost:8080/api/songs/search/begin/sdjfhs');
-    console.log('http://localhost:8080/api/songs/search/any/sdjfhs');
-    console.log('http://localhost:8080/api/songs/search/year/2027');
-    console.log('http://localhost:8080/api/songs/search/year/2019/2016');
-    console.log('http://localhost:8080/api/songs/artist/7834562');
-    console.log('http://localhost:8080/api/playlists/35362');
-    console.log('http://localhost:8080/api/mood/boo');
-    */
 });
